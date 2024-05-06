29f4e36f3638421db44f9255f4a91564ea726bba
AlexeyT74 <alexey.tal@gmail.com>
2024-05-06 23:21:14 +0300

    5

--- assignments/todolist-react/src/App.jsx
+++ assignments/todolist-react/src/App.jsx
@@ -1,26 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Todolist from './components/todolist'
-import TodoForm from './components/todoform'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Todolist from './components/todolist';
+import TodoForm from './components/todoform';
+import { todoFactory } from './components/todolist';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([todoFactory("First")]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    const inputValue = formData.get('input');
+    const inputValue = formData.get('title');
+    setTodos(todoFactory(inputValue));
   };
 
   return (
     <>
-    <h1>Hello Todo from React</h1>
-    <TodoForm addTodo={handleSubmit}/>
-    <Todolist/>
-  </>
-  )
+      <h1>Hello Todo from React</h1>
+      <TodoForm addTodo={handleSubmit} />
+      <Todolist todos={todos} />
+    </>
+  );
 }
 
-export default App
+export default App;
--- assignments/todolist-react/src/components/todolist.jsx
+++ assignments/todolist-react/src/components/todolist.jsx
@@ -1,28 +1,30 @@
-
-function Todolist(){
-    console.log("Todolist");
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    );
+function Todolist({ todos }) {
+  console.log('Todolist', todos);
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <span>Title: {todo.title}</span>
+          <span>Date: {todo.date}</span>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 function uuidv4() {
-    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
-      (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
-    );
-  }
-  
- export function todoFactory(title) {
-    return {
-      title,
-      id: uuidv4(),
-      done: false,
-    };
-  }
+  return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+    (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+  );
+}
 
+export function todoFactory(title) {
+  return {
+    title,
+    id: uuidv4(),
+    done: false,
+    date: Date.now(),
+  };
+}
 
-export default Todolist
+export default Todolist;
