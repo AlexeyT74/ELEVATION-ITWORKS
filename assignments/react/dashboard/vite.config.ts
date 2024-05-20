9179cc1efdd6d428be731e3a4f72037a878c464f
AlexeyT74 <alexey.tal@gmail.com>
2024-05-20 20:50:54 +0300

    xxx

--- assignments/react/dashboard/vite.config.ts
+++ assignments/react/dashboard/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ELEVATION-ITWORKS/', 
   plugins: [react()],
+  // base: "./"
+  base: "/ELEVATION-ITWORKS/"
 })
