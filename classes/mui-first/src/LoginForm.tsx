c725ec9f5c494ee1ed7e56d45a8b7e7a3524edd9
AlexeyT74 <alexey.tal@gmail.com>
2024-05-29 20:38:22 +0300

    fix

--- classes/mui-first/src/LoginForm.tsx
+++ classes/mui-first/src/LoginForm.tsx
@@ -1,14 +1,15 @@
-import { Button, Stack, TextField, Typography } from '@mui/material';
+import { Alert, Button, Card, TextField, Typography } from '@mui/material';
 
 export default function LoginForm() {
   return (
-    <Stack spacing={2} component={'form'}>
+    <Card component={'form'} sx={{p:'15px'}} >
       <Typography variant="h4">Login with MUI</Typography>
       <TextField label="Username" variant="outlined" margin="normal" fullWidth required />
       <TextField label="Password" variant="outlined" type="password" margin="normal" fullWidth required />
       <Button type="submit" variant="contained" color="primary">
         Submit
       </Button>
-    </Stack>
+      <Alert severity="error" variant="outlined" sx={{mt:'15px'}}>Something gone wrong</Alert>
+    </Card>
   );
 }
